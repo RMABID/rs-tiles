39971d5907b3f5afe3a442880a9b461a0666c4b8
RMABID <mdabidaliimran@gmail.com>
2025-01-25 15:52:18 +0600

    navbar commit

--- src/components/share/Navbar.jsx
+++ src/components/share/Navbar.jsx
@@ -1,12 +1,20 @@
-import { NavLink } from "react-router-dom";
+import { IoMenu } from "react-icons/io5";
+import { Link, NavLink } from "react-router-dom";
+import logo from "../../assets/logo/rs_logo-removebg-preview.png";
 
 const Navbar = () => {
   return (
-    <nav className="flex justify-evenly py-4">
+    <nav className="flex items-center fixed w-full z-40 justify-evenly py-4 Raleway bg-[#B8BACD]">
       <div>
-        <h1 className="text-2xl font-medium">RS</h1>
+        <Link to={"/"} className="items-center hidden lg:flex cursor-pointer">
+          <img className=" w-16 " src={logo} />
+          <h1 className="text-xl -ml-4 italic font-medium">RS Tiles</h1>
+        </Link>
+        <h1 className="text-xl md:hidden font-medium">
+          <IoMenu />
+        </h1>
       </div>
-      <ul className="flex items-center gap-4">
+      <ul className="hidden lg:flex items-center gap-4">
         <li>
           <NavLink to={"/"}>Home</NavLink>
         </li>
@@ -14,9 +22,13 @@ const Navbar = () => {
           <NavLink to={"/products"}>Products</NavLink>
         </li>
         <li>
-          <NavLink>About Us</NavLink>
+          <NavLink to={"/about"}>About Us</NavLink>
         </li>
       </ul>
+      <Link to={"/"} className="flex items-center lg:hidden cursor-pointer">
+        <h1 className="text-xl -mr-4 italic font-medium">RS Tiles</h1>
+        <img className=" w-16 " src={logo} />
+      </Link>
     </nav>
   );
 };
