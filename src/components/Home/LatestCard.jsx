740d8d776297faedfa8c2226262260e2da2e2705
RMABID <mdabidaliimran@gmail.com>
2025-02-06 16:40:44 +0600

    latest products swiper slider add

--- src/components/Home/LatestCard.jsx
+++ src/components/Home/LatestCard.jsx
@@ -3,6 +3,13 @@ import useAxiosPublic from "../../hooks/useAxiosPublic";
 import { useQuery } from "@tanstack/react-query";
 import { TiSocialFacebook } from "react-icons/ti";
 import { CiTwitter } from "react-icons/ci";
+// import Slider from "./Slider";
+
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+// import "swiper/css";
+// import "swiper/css/pagination";
+import { Pagination, Autoplay } from "swiper/modules";
 
 const LatestCard = () => {
   const axiosPublic = useAxiosPublic();
@@ -15,34 +22,69 @@ const LatestCard = () => {
   });
 
   return (
-    <div className="grid grid-cols-2 gap-8 my-14">
-      {products
-        .filter((product) => product.latest === true)
-        .map((item, index) => (
-          <Link key={index} to={`/all-products/${item?._id}`} className="w-96">
-            <div className="group  shadow w-96  rounded-xl hover:scale-105  duration-500 transition-all border border-white overflow-hidden bg-white flex flex-col">
-              <img
-                className="rounded-t-lg w-full h-full object-cover"
-                src={item?.product_image}
-                alt={item?.product_name}
-              />
-              <div className="bg-white  relative flex flex-col group-hover:-translate-y-11 w-full transition-all duration-500  rounded-b-lg py-4 flex-grow  items-center justify-center">
-                <h3 className="text-3xl text-[#1D86BD] text-center font-medium py-4">
-                  {item?.product_name}
-                </h3>
-                <div className="absolute flex  items-center -mb-4 gap-3 -bottom-full group-hover:bottom-1 transition-all duration-500">
-                  <span className="text-2xl mt-3 aspect-square border rounded-full p-1">
-                    <TiSocialFacebook />
-                  </span>
-                  <span className="text-2xl mt-3 aspect-square border rounded-full p-1">
-                    <CiTwitter />
-                  </span>
+    <div className="my-24">
+      <Swiper
+        slidesPerView={2}
+        spaceBetween={20}
+        autoplay={{
+          delay: 2500,
+          disableOnInteraction: false,
+        }}
+        pagination={{
+          clickable: true,
+        }}
+        breakpoints={{
+          "@0.00": {
+            slidesPerView: 1,
+            spaceBetween: 50,
+          },
+          "@0.75": {
+            slidesPerView: 2,
+            spaceBetween: 20,
+          },
+          "@1.00": {
+            slidesPerView: 2,
+            spaceBetween: 60,
+          },
+          "@1.50": {
+            slidesPerView: 4,
+            spaceBetween: 50,
+          },
+        }}
+        modules={[Pagination, Autoplay]}
+        className="mySwiper"
+      >
+        {products
+          .filter((product) => product.latest === true)
+          .map((item, index) => (
+            <SwiperSlide
+              key={index}
+              to={`/all-products/${item?._id}`}
+              className="w-full"
+            >
+              <div className="group  shadow w-96 h-[30rem] rounded-xl hover:scale-105  duration-500 transition-all border border-white overflow-hidden bg-white flex flex-col">
+                <img
+                  className="rounded-t-lg object-cover"
+                  src={item?.product_image}
+                  alt={item?.product_name}
+                />
+                <div className="bg-white  relative flex flex-col group-hover:-translate-y-11 w-full transition-all duration-500  rounded-b-lg py-4 flex-grow  items-center justify-center">
+                  <h3 className="text-3xl text-[#1D86BD] text-center font-medium py-4">
+                    {item?.product_name}
+                  </h3>
+                  <div className="absolute flex  items-center -mb-4 gap-3 -bottom-full group-hover:bottom-1 transition-all duration-500">
+                    <span className="text-2xl mt-3 aspect-square border rounded-full p-1">
+                      <TiSocialFacebook />
+                    </span>
+                    <span className="text-2xl mt-3 aspect-square border rounded-full p-1">
+                      <CiTwitter />
+                    </span>
+                  </div>
                 </div>
               </div>
-            </div>
-          </Link>
-        ))}
-      {/*  */}
+            </SwiperSlide>
+          ))}
+      </Swiper>
     </div>
   );
 };
--- src/components/Home/LatestProducts.jsx
+++ src/components/Home/LatestProducts.jsx
@@ -5,7 +5,7 @@ import LatestCard from "./LatestCard";
 
 const LatestProducts = () => {
   return (
-    <section className="my-14 w-10/12 lg:w-8/12 mx-auto ">
+    <section className="my-14 w-10/12 mx-auto ">
       <Fade direction="down">
         <Title text1={"Latest Products"} />
       </Fade>
